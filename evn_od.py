316f404fabe3b1664641b0725cc326620251bc0a
MHassan1104 <muhammadhassansdk@gmail.com>
2025-05-09 14:30:25 +0500

    Find Evn or Odd num

---
+++ evn_od.py
@@ -0,0 +1,7 @@
+# Even or Odd: Write a program to check if a number is even or odd.
+
+a=int(input("Enter a number to check Even or Odd: = "))
+if a % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
